008f48818a44e3ee7c96e7064e0f607633649cc6
Phan Hoang An <anhoangphan96@gmail.com>
2023-10-18 02:46:33 +0700

    Update section 20

--- section20-21/section20/django_level_five/learning_users/learning_users/settings.py
+++ section20-21/section20/django_level_five/learning_users/learning_users/settings.py
@@ -139,3 +139,6 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# LOGIN URL
+LOGIN_URL = "/basic_app/login"
