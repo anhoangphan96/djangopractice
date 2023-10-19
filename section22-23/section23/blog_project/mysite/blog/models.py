1205cd3fb62e1ef9f192a7c9a09ea8d668254735
Phan Hoang An <anhoangphan96@gmail.com>
2023-10-19 09:56:20 +0700

    Fix all bug

--- section22-23/section23/blog_project/mysite/blog/models.py
+++ section22-23/section23/blog_project/mysite/blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     title = models.CharField(max_length=256)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now, null=True)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
